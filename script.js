bce335f3bdd798661521306e7caa3081b01ab469
Somiya Ratna Behera <beherasomiyaratna@gmail.com>
2023-11-02 16:44:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+let btn = document.getElementById("enterBtn");
+let p = document.getElementById("status");
+let h1 = document.createElement("h1");
+let body = document.querySelector("body");
+
+btn.addEventListener("click", () => {
+  p.remove();
+  body.appendChild(h1);
+  h1.innerText = "Entered Metaverse";
+});
